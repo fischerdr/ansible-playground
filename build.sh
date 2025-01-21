42d71720896696709bd12d3cd8adcb6282eec3ad
David Fischer <fischer.d.r@gmail.com>
2025-01-20 18:36:47 -0700

    update req and linter

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
- ansible-builder build -v3 -t quay.io/ansible/awx-ee # --container-runtime=docker # Is podman by default
- ### build ee environment
- docker buildx create --name test-ee
- docker buildx use test-ee
- ansible-builder create -v3 --output-file=Dockerfile
- docker buildx build --load --tag=test-ee:latest context
- 
+ #!/bin/bash
+
+# Define variables
+BUILDER_TAG="quay.io/ansible/awx-ee"
+EE_TAG="test-ee:latest"
+BUILDX_NAME="test-ee"
+
+# Build execution environment
+ansible-builder build -v3 -t "${BUILDER_TAG}" --container-runtime=docker
+
+# Build ee environment
+docker buildx create --name "${BUILDX_NAME}"
+docker buildx use "${BUILDX_NAME}"
+ansible-builder create -v3 --output-file="Dockerfile"
+docker buildx build --load --tag="${EE_TAG}" context

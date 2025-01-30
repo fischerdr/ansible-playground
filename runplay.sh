2ec082791a7ad132378bd1d3ce6c7eafb7c92c1c
David Fischer <fischer.d.r@gmail.com>
2025-01-30 16:00:41 -0700

    update ansible crap

--- runplay.sh
+++
@@ -1,5 +0,0 @@
-ansible-playbook k8s_vault_sa_setup.yml \
-  -e cluster_name=your-cluster-name \
-  -e vault_token=your-vault-token \
-  -e vault_namespace=your-vault-namespace \
-  -e k8s_namespace=your-k8s-namespace
